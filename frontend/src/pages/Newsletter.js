cf729533e7b114bae9296f9645b205c8240fd29a
Tolik Petras <toliketras4@gmail.com>
2023-08-02 13:29:29 +0300

    frontend

---
+++ frontend/src/pages/Newsletter.js
@@ -0,0 +1,18 @@
+import NewsletterSignup from '../components/NewsletterSignup';
+import PageContent from '../components/PageContent';
+
+function NewsletterPage() {
+  return (
+    <PageContent title="Join our awesome newsletter!">
+      <NewsletterSignup />
+    </PageContent>
+  );
+}
+
+export default NewsletterPage;
+
+export async function action({ request }) {
+  const data = await request.formData();
+  const email = data.get('email');
+  return { message: 'Signup successful!' };
+}
